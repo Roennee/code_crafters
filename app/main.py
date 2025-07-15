256d877ccc358b83e8a74adccc2bb69a8474b005
Bucher Raffael <raffael.bucher@w-h.ch>
2025-07-15 15:43:10 +0200

    socket test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import socket  # noqa: F401
 
-server_socket = socket.create_server(("localhost", 4221))
+server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
 connection, address = server_socket.accept() # wait for client
 print(f"accepted connecton from {address}")
@@ -9,6 +9,8 @@ print(f"accepted connecton from {address}")
 data = connection.recv(1024)
 
 #send same data back 
-connection.sendall(data)
+connection.sendall("HTTP/1.1 200 OK\r\n\r\n")
 
 
+if __name__ == "__main__":
+    main()
